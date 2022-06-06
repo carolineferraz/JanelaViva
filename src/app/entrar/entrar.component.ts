7943dc0ebe63783e90969fff245be1a1a707a577
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-06-06 09:58:18 -0300

    feat: implementando serviço de registrar/entrar e alterando componente Entrar para fazer login pelo form

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  usuarioLogin: UsuarioLogin = new UsuarioLogin();
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+
+  }
+
+  entrar() {
+    this.authService.entrar(this.usuarioLogin).subscribe({
+      next: ((res: UsuarioLogin) => {
+        this.usuarioLogin = res;
+        environment.token = this.usuarioLogin.token;
+        environment.id = this.usuarioLogin.id;
+        environment.nome = this.usuarioLogin.nome;
+        environment.avaliacao = this.usuarioLogin.avaliacao;
+        environment.habilidades = this.usuarioLogin.habilidades;
+
+        console.log({ ...environment })
+      }),
+      error: err => {
+        if (err.status === 401) {
+          alert('Usuário/Senha incorretos')
+        }
+      }
+    })
   }
 
 }
